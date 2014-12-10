2eebb4d50e7ae2c9887bdc18ec99aa810cf2b34d
Jonas Karppinen <jonask@cs.umu.se>
2014-12-10 21:12:46 +0100

    removed misc.el

--- misc.el
+++
@@ -1,8 +0,0 @@
-;; Bibtex in org-mode
-(defun org-mode-reftex-setup ()
-(load-library "reftex")
-(and (buffer-file-name)
-(file-exists-p (buffer-file-name))
-(reftex-parse-all))
-(define-key org-mode-map (kbd "C-c )") 'reftex-citation))
-(add-hook 'org-mode-hook 'org-mode-reftex-setup)
